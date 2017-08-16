6cb68cf2363b5dd02feccd1a8e2ff8532b1338c8
jasmine <jazzmithani@gmail.com>
2017-08-16 19:08:53 -0400

    general update

--- js/part1.js
+++ js/part1.js
@@ -1,41 +1,71 @@
 var width = 800;
 var height = 600;
 
-var squareSize = 50;
+var mouseSize = 20;
+var mousePadding = 2;
+var rowLen = 10; // how many mice per row
 
-var svg = d3.select("body")
+var svg = d3.select("#graph")
             .append("svg")
             .attr("width", width)
             .attr("height", height);
-
+/*
 var rect = svg.append("rect")
             .attr("height", squareSize)
             .attr("width", squareSize)
             .attr("x", 10)
             .attr("y", 10);
+            */
 
-d3.csv("csv/all_films.csv", function(data) {
-  //console.log(data);
-  dataset = data;
+d3.csv("../csv/all_films_nosequels.csv", function(data) {
 
-  var numberFilms = dataset.length;
-  //console.log(numberFilms);
+  console.log(data);
 
-  svg.selectAll("rect")
-    .data(dataset)
+  var miceData = makeObjects(data);
+  console.log(miceData);
+
+  svg.selectAll(".mouse")
+    .data(miceData)
     .enter()
     .append("rect")
-    .attr("height", squareSize)
-    .attr("width", squareSize)
-    .attr("y", "50%")
-    .attr("x", function(d, i) {
-      var x = (i * 50) + 2;
-      console.log(x)
-      return x;
-    });
-
-
-
+    .classed("image", true)
+    .attr("height", mouseSize)
+    .attr("width", mouseSize)
+    .attr("x", function(d) { return d.x; })
+    .attr("y", function(d) { return d.y; });
 });
+
+function makeObjects(dataset) {
+  //console.log(data);
+
+  return dataset.map(function(d, i) {
+    //console.log(d);
+    //console.log(i);
+    d.title = d.title; // string
+    d.date = d.date; // keeping as a string right now
+    d.studio = d.studio; // string
+    d.mom = function(d) {
+      if (d.mom === 1) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }; // bool
+    d.dad = function(d) {
+      if (d.dad === 1) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }; // bool
+    d.x = (i % rowLen) * (mouseSize + mousePadding); // int
+    //console.log(d.x);
+    d.y = Math.floor(i / (mouseSize + mousePadding)) * (mouseSize + mousePadding); // int
+    return d; // new object
+    //console.log(d.y);
+  });
+}
 
 // var squares = svg.selectAll("rect")
