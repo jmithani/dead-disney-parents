9f0cc09f74459c0f8618fb1995735909f357c707
jasmine <jazzmithani@gmail.com>
2017-08-23 16:47:50 -0400

    buttons styled and update mice and sidebar on click

--- js/part1.js
+++ js/part1.js
@@ -16,17 +16,23 @@ var buttons = [{ "name": "mom", "count": 30, "desc": "of films have a missing mo
                { "name": "dad", "count": 20, "desc": "of films have a missing father"  },
                { "name": "both", "count": 20, "desc": "of films have neither parent present" }];
 
-var btns = d3.select("#controls").selectAll("button")
+d3.select("#controls").selectAll("button")
   .data(buttons)
   .enter()
   .append("button")
+  .classed("btn-style", true)
   .attr("height", 70)
   .attr("width", 200)
   .text( function(d, i) {
     return d.name;
   });
 
-  btns.on("click", updateSide);
+// When a button is clicked, the sidebar and the mice update
+d3.select("#controls").selectAll("button")
+  .on("click", function(d) {
+    updateSide(d);
+    updateMice(d);
+  });
 
 /***** Reading in CSV data in order to make mice grid *****/
 d3.csv("../csv/all_films_nosequels.csv", function(data) {
@@ -41,13 +47,16 @@ d3.csv("../csv/all_films_nosequels.csv", function(data) {
     .data(miceData)
     .enter()
     .append("image")
-    .classed("image", true)
+    .classed("mouse", true)
     .attr("href", "/img/mouse.svg")
     .attr("height", mouseSize)
     .attr("width", mouseSize)
     .attr("x", function(d) { return d.x; })
-    .attr("y", function(d) { return d.y; });
+    .attr("y", function(d) { return d.y; })
+    .each(addClasses);
 });
+
+/***** DATA FORMATTING FUNCTIONS *****/
 
 // Function to format raw data set into objects
 function makeObjects(dataset) {
@@ -67,11 +76,30 @@ function makeObjects(dataset) {
   });
 }
 
+// Function to add classes based on data object
+function addClasses(d) {
+  //console.log(this);
+  if (d.mom == true) {
+    // console.log("mom true");
+    d3.select(this).classed("mom-dead", true);
+  }
+
+  if (d.dad == true) {
+    // console.log("dad true");
+    d3.select(this).classed("dad-dead", true);
+  }
+
+  if (d.mom == true && d.dad == true) {
+    console.log(d.title);
+    d3.select(this).classed("both", true);
+  }
+}
+
 /***** Functions for button filtering *****/
 
 // Function to change the text of the sidebar
 function updateSide(d) {
-  
+  console.log("updating side");
   var elDesc = document.getElementById("description");
   var elCount = document.getElementById("count");
   elCount.innerHTML = d.count;
@@ -79,4 +107,41 @@ function updateSide(d) {
 
 }
 
-// Function to update the mice color based on the
+// Function to update the mice color based on the button clicked
+function updateMice(d) {
+  // Right now, .active isn't mapped to anything, it's for debugging
+
+  var opacity = 0.3;
+  console.log("updating mice");
+
+  d3.selectAll(".mouse")
+    .classed("active", false)
+    .attr("href", "/img/mouse.svg")
+    .attr("opacity", opacity);
+
+  switch(d.name) {
+    case "both":
+      console.log("matched both");
+      d3.selectAll(".both")
+        .classed("active", true)
+        .attr("opacity", 1)
+        .attr("href", "/img/mouse-pink.svg");
+      break;
+
+    case "mom":
+      console.log("matched mom");
+      d3.selectAll(".mom-dead")
+        .classed("active", true)
+        .attr("opacity", 1)
+        .attr("href", "/img/mouse-pink.svg");
+      break;
+
+    case "dad":
+      console.log("matched dad");
+      d3.selectAll(".dad-dead")
+        .classed("active", true)
+        .attr("opacity", 1)
+        .attr("href", "/img/mouse-pink.svg");
+      break;
+  }
+}
